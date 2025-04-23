a002d3c8b054321487ccae115ede2d5e1bb36acb
Fathima225 <fathimaniha225@gmail.com>
2025-04-23 09:46:36 +0530

    Add project files.

---
+++ day07/day07/prg01.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Hello Fathima" << endl;
+	return 0;
+}
---
+++ day07/day07/prg02.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "This is prg02.cpp" << endl;
+	return 0;
+}
